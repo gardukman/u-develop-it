f0dab3ba02b9540c58c29385e26b469f33a959e0
Duke Nukem <garrettduke87@gmail.com>
2020-10-15 17:50:37 -0600

    added a server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello world'
+//     });
+// });
+
+// function that will start the express.js server on port 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
